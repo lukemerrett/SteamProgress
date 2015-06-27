e185f9b4c610735df6039992d66ba59802935063
Luke Merrett <lukeamerrett@gmail.com>
2015-06-27 17:17:41 +0100

    Updated the tests to match the new logic

--- unittests/playtimetests.py
+++ unittests/playtimetests.py
@@ -12,10 +12,6 @@ class PlaytimeTests(unittest.TestCase):
         playtime_in_minutes = playtime.get_total_playtime_ever()
         self.assertTrue(playtime_in_minutes > 0)
 
-    def test_get_list_of_games_never_played_returns_list_of_games(self):
-        games_never_played = playtime.get_list_of_games_never_played()
-        self.assertTrue(isinstance(games_never_played, list))
-
-    def test_choose_a_random_never_played_game_to_play_returns_a_game(self):
-        game_to_play = playtime.choose_a_random_never_played_game_to_play()
+    def test_choose_a_random_game_to_play_returns_a_game(self):
+        game_to_play = playtime.choose_a_random_game_to_play(False, False)
         self.assertTrue(isinstance(game_to_play, str))
