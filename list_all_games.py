073ae656e2250744455176f2e34e8335addf9d59
Luke Merrett <lukeamerrett@gmail.com>
2015-06-27 18:17:06 +0100

    Added a script to list all games within an account

---
+++ list_all_games.py
@@ -0,0 +1,11 @@
+from clients import installed_games, steamapi
+
+if __name__ == '__main__':
+    api = steamapi.SteamApiClient()
+    games = api.get_player_owned_games()
+    for game in games:
+        game['installed'] = installed_games.is_game_in_installed_games_list(game)
+
+    for game in sorted(games, key=lambda game: game['name']):
+        installed = 'Yes' if game['installed'] else 'No'
+        print ('Name: "' + game['name'] + '" Installed: ' + installed)
