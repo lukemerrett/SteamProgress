da19b140a41f83f91befda98a174cd1185ffb162
Luke Merrett <lukeamerrett@gmail.com>
2015-06-27 17:36:09 +0100

    Changed the test verbosity Gives better error output this way

--- runtests.py
+++ runtests.py
@@ -3,4 +3,4 @@ import unittest
 if __name__ == "__main__":
     # Finds all tests in submodules ending in *tests.py and runs them
     suite = unittest.TestLoader().discover('.', pattern = "*tests.py")
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.TextTestRunner().run(suite)
