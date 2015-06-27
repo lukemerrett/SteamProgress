7da4e4aca560640435d88c346f6e2fa9c8a54a23
Luke Merrett <lukeamerrett@gmail.com>
2015-06-27 17:36:41 +0100

    Now filters out games that arent installed

--- analytics/playtime.py
+++ analytics/playtime.py
@@ -1,6 +1,8 @@
 __author__ = 'Luke Merrett'
 
+import os
 import random
+import settings
 from clients.steamapi import SteamApiClient
 
 def get_total_playtime_for_last_two_weeks():
@@ -29,16 +31,23 @@ def choose_a_random_game_to_play(choose_never_played, choose_installed):
 
     if choose_never_played:
         games_never_played = []
-
         for game in games:
             if 'playtime_forever' in game:
                 if game['playtime_forever'] == 0:
                     games_never_played.append(game)
-
         games = games_never_played
 
     if choose_installed:
-        pass
+        installed_games = []
+        installed_game_folders = os.listdir(settings.steam_user_folder)
+        for game in games:
+            appid = str(game['appid'])
+            if appid in installed_game_folders:
+                installed_games.append(game)
+        games = installed_games
+
+    if games.__len__() == 0:
+        return 'Could not find any games you can play!'
 
     random_int = random.randrange(games.__len__())
     return games[random_int]['name']
