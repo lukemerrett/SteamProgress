272c3d85d0e58ba99af2d1ef2383f21d447e67d2
Luke Merrett <lukeamerrett@gmail.com>
2015-07-02 22:57:00 +0100

    Added a setup.py script

---
+++ setup.py
@@ -0,0 +1,14 @@
+__author__ = 'Luke Merrett'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="SteamProgress",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Luke Merrett",
+    author_email="lukeamerrett@gmail.com",
+    url='https://github.com/lukemerrett/SteamProgress',
+    license="MIT",
+)
